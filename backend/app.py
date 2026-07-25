7c286830edd589fb62548d3e60315153596c3e47
Keerthi-Badri <badrikeerthi18@gmail.com>
2026-07-25 15:44:45 +0530

    Fix Railway database import

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from backend.database import get_connection
+from database import get_connection
 
 from flask_jwt_extended import (
     JWTManager,
